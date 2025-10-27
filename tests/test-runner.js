abf9430582a6972b6a74fe950b09076cf5c42d6e
CurtMinyard <cminyard@perseverenow.org>
2025-10-27 12:36:32 -0500

    new chi

--- tests/test-runner.js
+++ tests/test-runner.js
@@ -1,19 +1,24 @@
 const chai = require('chai');
-const assert = chai.assert;
 const chaiHttp = require('chai-http');
-const server = require('../server'); // your server.js
+const server = require('../server');
+const assert = chai.assert;
+
 chai.use(chaiHttp);
 
-suite('Functional Tests', function () {
+describe('Functional Tests', function () {
   this.timeout(5000);
 
-  test('GET / should render Pug template', function (done) {
-    chai.request(server)
-      .get('/')
-      .end(function (err, res) {
-        assert.equal(res.status, 200);
-        assert.match(res.text, /Hello Pug!/);
-        done();
-      });
+  describe('Template Engine Tests', function () {
+    it('GET / should render Pug template', function (done) {
+      chai.request(server)
+        .get('/')
+        .end(function (err, res) {
+          if (err) return done(err);
+          assert.equal(res.status, 200);
+          assert.match(res.text, /Hello Pug!/);
+          assert.match(res.text, /Home Page/);
+          done();
+        });
+    });
   });
 });
